1878dc217f18e4fa13eddbb56e5d74b1d0ed9ad3
TSsec0x62 <51350302+TSsec0x62@users.noreply.github.com>
2019-06-05 00:29:44 +1000

    Added use of command line args
    The file for the password list and for the encrypted password can now be passed in through the command line

--- SHA2BF.py
+++ SHA2BF.py
@@ -14,19 +14,20 @@ and your password list must be in a file called 'password_list'
 
 #importing libraries
 import hashlib
+import argparse
 
 #reads in the encrypted password that you are cracking and cleans it
-def read_encrypted_pass():
-	encrypted_pass = open(r"encrypted_pass", "r")
+def read_encrypted_pass(encrypted_pass_file):
+	encrypted_pass = open(encrypted_pass_file, "r")
 	e_p = encrypted_pass.readlines()
 	string_e_p = e_p[0]
 	string_e_p = string_e_p.strip()
 	return string_e_p
 
 #compares the encrypted password against a password list
-def compare_to_passlist(encrypted_password):
+def compare_to_passlist(encrypted_password, password_list):
 	counter = 0
-	password_list = open(r"password_list", "r")
+	password_list = open(password_list, "r")
 	for line in password_list:
 		line = str(line)
 		line = line.strip()
@@ -38,8 +39,16 @@ def compare_to_passlist(encrypted_password):
 			print("Password succesfully cracked after " + str(counter) + " attempts!")
 			print("The password is: " + line)
 
+def parse_in_arguments():
+	parser = argparse.ArgumentParser(description = 'Take in the file names', usage="python3 SHA2BF.py [path/to/password_list] [path/to/encrypted_password]")
+	parser.add_argument('pl', action="store", type=str, help='password list file path')
+	parser.add_argument('ep', action="store", type=str, help='encrypted password file path')
+	args = parser.parse_args()
+	return args
+
 def main():
-	encrypted_password = read_encrypted_pass()
-	compare_to_passlist(encrypted_password)
+	args = parse_in_arguments()
+	encrypted_password = read_encrypted_pass(args.ep)
+	compare_to_passlist(encrypted_password, args.pl)
 
 main()
